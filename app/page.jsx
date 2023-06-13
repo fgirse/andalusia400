d86ef7a480ebb139b9b1c0d320e8c60cfe214dc9
fgirse <fgirse@bluemail.ch>
2023-06-13 14:42:49 +0200

    "3er commit"

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Team from "@sections/Team";
 const Home = () => (
    <>
   <section className='bg-[url("/assets/images/andalusiaRedRoofs.jpg")]
-   bg-cover bg-no-repeat h-[95vh]
+   bg-cover bg-no-repeat h-[90vh]
    w-full flex-center flex-col rounded-2xl'>
     <div className="mt-[-10vh] bg-gray-300/10 rounded-2xl px-3 py-3">
     <h1 className='head_text text-center'>
