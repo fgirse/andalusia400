19375a5a15bb611a8eac2bd9ca8ae0ebdf7a1582
fgirse <fgirse@bluemail.ch>
2023-06-14 17:59:36 +0200

    ""

--- sections/Team.jsx
+++ sections/Team.jsx
@@ -3,28 +3,29 @@ import Image from 'next/image';
 import { archidaught } from '../utils/fonts';
 
 function Team() {
+   
     return (
         <div>
-            <div className="bg-blue-600                                                                                                                                 container flex justify-center mx-auto pt-16">
+            <div className="container flex justify-center mx-auto pt-16">
                 <div>
-                    <p className="font-archidaught text-green-500 text-lg text-center font-normal pb-3">ANDALUSIA TEAM</p>
+                    <p className="text-gray-500 text-lg text-center font-normal pb-3">TEAM</p>
                     <h1 className="xl:text-4xl text-3xl text-center text-gray-800 font-extrabold pb-6 sm:w-4/6 w-5/6 mx-auto">The Talented People Behind the Scenes of the Organization</h1>
                 </div>
             </div>
             <div className="w-full bg-gray-100 px-10 pt-10">
-                <div className="bg-red-rzzcontainer mx-auto">
+                <div className="container mx-auto">
                     <div className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around">
-                        <div className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
-                            <div className="rounded overflow-hidden shadow-md bg-red-500">
+                        <div className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-1/5">
+                            <div className="rounded overflow-hidden shadow-md bg-white">
                                 <div className="absolute -mt-20 w-full flex justify-center">
-                                    <div className="h-24 w-24">
-                                        <img src="assets/images/albers.jpg" alt="PortraitNadine" className="rounded-full object-cover h-full w-full shadow-md" />
+                                    <div className="h-32 w-32">
+                                    <img src="assets/images/albers.jpg" alt="PortraitNadine" className="rounded-full object-cover h-full w-full shadow-md" />
                                     </div>
                                 </div>
                                 <div className="px-6 mt-16">
-                                    <div font-archidaught>Nadine</div>
-                                    <p className="text-gray-800 text-sm text-center">Chief Financiel Officer</p>
-                                    <p className="text-center text-gray-600 text-base pt-3 font-normal">Nadine ist eine ausgesprochen erfahrene Kennerin des Immobilienmarktes in Andalusien.... </p>
+                                    <div className="font-bold text-3xl text-center pb-1">Nadine</div>
+                                    <p className="text-gray-800 text-sm text-center">CFO</p>
+                                    <p className="text-center text-gray-600 text-base pt-3 font-normal">Nadine ist eine erfahrene Kennnerin des Immobilienmarktes in Andalusien...</p>
                                     <div className="w-full flex justify-center pt-5 pb-5">
                                         <a href="javascript:void(0)" className="mx-5">
                                             <div>
