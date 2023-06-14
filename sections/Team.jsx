53955bba283a81b2a0dede4528731b120e40371f
fgirse <fgirse@bluemail.ch>
2023-06-14 19:39:01 +0200

    "Modifikation new template"

--- sections/Team.jsx
+++ sections/Team.jsx
@@ -95,7 +95,7 @@ function Team() {
                         </div>
                         
 
-                        <div className="xl:w-1/4 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                        <div className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
                             <div className="rounded overflow-hidden shadow-md bg-white">
                                 <div className="absolute -mt-20 w-full flex justify-center">
                                     <div className="h-32 w-32">
