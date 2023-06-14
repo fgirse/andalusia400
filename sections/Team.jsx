f46ae06bb24bc3ebc16432542a4da98eb23fc460
fgirse <fgirse@bluemail.ch>
2023-06-14 18:18:01 +0200

    ""

--- sections/Team.jsx
+++ sections/Team.jsx
@@ -58,7 +58,7 @@ function Team() {
                             <div className="rounded overflow-hidden shadow-md bg-white">
                                 <div className="absolute -mt-20 w-full flex justify-center">
                                     <div className="h-32 w-32">
-                                    <img src="assets/images/albers.jpg" alt="PortraitNadine" className="rounded-full object-cover h-full w-full shadow-md" />
+                                    <img src="assets/images/albers.jpg" alt="PortraitBernhard" className="rounded-full object-cover h-full w-full shadow-md" />
                                     </div>
                                 </div>
                                 <div className="px-6 mt-16">
@@ -95,11 +95,11 @@ function Team() {
                         </div>
                         
 
-                        <div className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                        <div className="xl:w-1/4 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
                             <div className="rounded overflow-hidden shadow-md bg-white">
                                 <div className="absolute -mt-20 w-full flex justify-center">
                                     <div className="h-32 w-32">
-                                        <img src="assets/images/albers.jpg" alt className="rounded-full object-cover h-full w-full shadow-md" />
+                                        <img src="assets/images/albers.jpg" alt="PortraitFrank" className="rounded-full object-cover h-full w-full shadow-md" />
                                     </div>
                                 </div>
                                 <div className="px-6 mt-16">
